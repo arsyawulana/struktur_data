f639f5a971d46f496cf4304caed5b37b1f7e61d9
root <root@LAPTOP-EM0TEMHK.>
2025-04-30 14:39:30 +0700

    catatan pertemuan 4

--- pert4/cpp/logic_building/looping_sum.cpp
+++ pert4/cpp/logic_building/looping_sum.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int findSum(int n)
 {
-    return n * (n + 1) / 2
+   return n * (n + 1) / 2;
 }
 
-int main(){
-    int n = 5;
-    cout << findSum(n);
-    return 0;
-}
+int main()
+{
+  int n = 5;
+  cout << findSum(n);
+  return 0;
+} 
